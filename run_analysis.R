29f5331f8bbbcff3a7dc9036f923cc8507a225ac
Liene Malkalne <73543001+liene-ma@users.noreply.github.com>
2021-03-02 20:57:19 +0000

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,11 @@
+setwd("/Users/macbook/Getting/Data/UCI HAR Dataset")
+
+library(plyr)
+library(data.table)
+subjectTrain = read.table('./train/subject_train.txt',header=FALSE)
+xTrain = read.table('./train/x_train.txt',header=FALSE)
+yTrain = read.table('./train/y_train.txt',header=FALSE)
+
+subjectTest = read.table('./test/subject_test.txt',header=FALSE)
+xTest = read.table('./test/x_test.txt',header=FALSE)
+yTest = read.table('./test/y_test.txt',header=FALSE)
